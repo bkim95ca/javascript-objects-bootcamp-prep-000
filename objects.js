c47d6d2fe76f9f060fff0da5d0e57e1d0ea6c611
Brandon Kim <bkim95ca@gmail.com>
2018-02-28 23:10:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,6 @@
+Object.assign({}, { foo: 'bar' })
+Object.assign({ eggs: 3 }, { flour: '1 cup' })
+Object.assign({ eggs: 3 }, { chocolate: '1 cup', flour: '2 cups' }, { flour: '1/2 cup' })
 function updateObjectWithKeyAndValue(obj, key, value) {
 
   return Object.assign({}, obj, { [key]: value })
