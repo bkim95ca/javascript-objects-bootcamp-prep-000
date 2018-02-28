75284f01d7f9476c7ff439abe1e39341d370c233
Brandon Kim <bkim95ca@gmail.com>
2018-02-28 23:36:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,3 @@
 var playlist = {
   artistName: "songTitle"
 }
-
-function updateObjectWithObject(targetObject, updatesObject) {
-  return Object.assign({}, targetObject, updatesObject)
-}
-
-const playlist = { artistName: "songTitle"}
-
-updatePlaylist(playlist)
