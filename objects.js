33068e318bf5da11b965064d130eb2295208e240
Brandon Kim <bkim95ca@gmail.com>
2018-02-28 23:13:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,1 @@
-Object.assign({}, { foo: 'bar' })
-Object.assign({ eggs: 3 }, { flour: '1 cup' })
-Object.assign({ eggs: 3 }, { chocolate: '1 cup', flour: '2 cups' }, { flour: '1/2 cup' })
-function updateObjectWithObject(targetObject, updatesObject) {
-
-  return Object.assign({}, targetObject, updatesObject)
-}
-
-
-const recipe = { eggs: 3 }
-
-updateObjectWithObject(recipe, 'chocolate', '1 cup')
-
-console.log(recipe)
+var meals = { breakfast: "oatmeal", lunch: "turkey sandwich", dinner: "steak and potatoes"}
