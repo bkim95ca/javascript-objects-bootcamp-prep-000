37c8ef465b59e10d3bf21aac112af1c3c83fb3d5
Brandon Kim <bkim95ca@gmail.com>
2018-02-28 23:49:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,8 +2,8 @@ var playlist = {
   artistName: "songTitle"
 }
 
-function updatePlaylist(obj, key, value)
-  return Object.assign({}, obj, { [key]: value})
+function updatePlaylist(playlist, artistName, songTitle)
+  return Object.assign({}, playlist, { [artistName]: songTitle})
 }
 
 updatePlaylist('Phil Ochs', "Here's to the state of Mississippi")
