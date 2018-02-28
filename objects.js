1d0b12a8161113c42de8d668d7c7552e378ebf7a
Brandon Kim <bkim95ca@gmail.com>
2018-02-28 23:09:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,20 +1,4 @@
-/*const firstMeal = 'lunch'
+function updateObjectWithKeyAndValue(obj, key, value) {
 
-var meals = {
-  breakfast: 'oatmeal',
-  lunch: 'burrito',
-  dinner: 'steak'
+  return Object.assign({}, obj, { [key]: value })
 }
-
-meals['second breakfast'] = 'bagel';
-meals.breakfast = 'cereal'
-
-console.log(meals['breakfast']) */
-
-Object.assign({}, { foo: 'bar'})
-
-Object.assign({ eggs: 3 }, { flour: '1 cup' })
-
-Object.assign({ eggs: 3 }, { chocolate: '1 cup', flour: '2 cups'}, { flour: '1/2 cup' })
-
-console.log(object.assign)
