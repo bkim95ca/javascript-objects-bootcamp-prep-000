fba47498eeb04bee1f711895ec94e50fc3fb5abb
Brandon Kim <bkim95ca@gmail.com>
2018-02-28 23:07:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -17,4 +17,4 @@ Object.assign({ eggs: 3 }, { flour: '1 cup' })
 
 Object.assign({ eggs: 3 }, { chocolate: '1 cup', flour: '2 cups'}, { flour: '1/2 cup' })
 
-console.log(object)
+console.log(recipe)
