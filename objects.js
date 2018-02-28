3c63907866290386285a1e15632650bbe478667e
Brandon Kim <bkim95ca@gmail.com>
2018-02-28 23:06:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -15,4 +15,4 @@ Object.assign({}, { foo: 'bar'})
 
 Object.assign({ eggs: 3 }, { flour: '1 cup' })
 
-Object.assign(P eggs: 3 ), { chocolate: '1 cup', flour: '2 cups'}, { flour: '1/2 cup' })
+Object.assign({ eggs: 3 }), { chocolate: '1 cup', flour: '2 cups'}, { flour: '1/2 cup' })
