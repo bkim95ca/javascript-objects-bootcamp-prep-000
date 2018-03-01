1a31da641f318d6153a90931c74fa714ef3d2491
Brandon Kim <bkim95ca@gmail.com>
2018-03-01 00:01:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@ updatePlaylist('Phil Ochs', "Here's to the state of Mississippi")
 
 
 function removeFromPlaylist(playlist, artistName) {
-  return Object.assign({ artistName: "songTitle" }, 'artistName')
+  return Object.assign({ artistName: "songTitle" }, "Kanye")
 
 }
 
